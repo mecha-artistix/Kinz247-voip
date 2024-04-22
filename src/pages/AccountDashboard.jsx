157f9ad0c23d13df2b837b8a1bf479b3a8e73fdb
huzaifa <mecha.artistix@gmail.com>
2024-04-22 17:06:27 +0500

    Update 21/4

--- src/pages/AccountDashboard.jsx
+++ src/pages/AccountDashboard.jsx
@@ -1,45 +1,27 @@
 import { FakeTable } from "../assets/FakeTable";
 
 function AccountDashboard() {
-  console.log(FakeTable);
   return (
     <>
-      <div className=" h-[800px]  overflow-auto">
-        <table className="h-144  table-fixed border-spacing-1  border border-k_dark_gray">
+      <div className="h-full overflow-auto  border">
+        <table className="h-144  table-fixed">
           <thead>
             <tr>
               <th>Account Name</th>
               <th>Account ID</th>
               <th>Balance</th>
-              <th>Gain Loss %</th>
               <th>ASR</th>
               <th>SDP</th>
               <th>Spent</th>
-              <th>Gain Loss %</th>
               <th>Profit</th>
               <th>Margin</th>
               <th>Sessions</th>
               <th>Gain Loss %</th>
-              <th>ASR</th>
             </tr>
           </thead>
           <tbody>
-            {FakeTable.map((row, index) => (
-              <tr key={index} className="text-center">
-                <td>{row.accountName}</td>
-                <td>{row.accountID}</td>
-                <td>{row.balance}</td>
-                <td>{row.asr}</td>
-                <td>{row.sdp}</td>
-                <td>{row.spent}</td>
-                <td>{row.accountID}</td>
-                <td>{row.profit}</td>
-                <td>{row.sdp}</td>
-                <td>{row.margin}</td>
-                <td>{row.sessions}</td>
-                <td>{row.profit}</td>
-                <td>{row.gainLoss}</td>
-              </tr>
+            {FakeTable.map((row) => (
+              <TableRow row={row} />
             ))}
           </tbody>
         </table>
@@ -49,3 +31,40 @@ function AccountDashboard() {
 }
 
 export default AccountDashboard;
+
+function TableRow({ row, indent = 0 }) {
+  const padding = 20 * indent + "px";
+  const childKey = Object.keys(row).find((key) => Array.isArray(row[key]));
+  return (
+    <>
+      <tr>
+        {Object.entries(row).map(([key, value], index) => {
+          if (key !== childKey) {
+            return (
+              <td
+                style={index === 0 ? { paddingLeft: padding } : {}}
+                key={key}
+                className="border-b"
+              >
+                {value}
+              </td>
+            );
+          }
+          return null;
+        })}
+      </tr>
+      {childKey &&
+        row[childKey].map((childRow, index) => (
+          <tr key={`child-${index}`}>
+            <td colSpan="100%">
+              <table className="w-full">
+                <tbody>
+                  <TableRow row={childRow} indent={indent + 1} />
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        ))}
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         k_dark_gray: "rgba(115, 115, 115, 1)",
         k_light_gray: "rgba(209, 209, 209, 1)",
         k_blue: "rgba(28, 119, 246, 1)",
+        k_light_blue: "rgba(28, 119, 246, 0.5)",
         k_dark_charcol: "rgba(51, 51, 51, 1)",
       },
     },
