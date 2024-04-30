d4682c0972a015ca3ee73e49eb7aa9c01aa7820a
mecha-artistix <mecha.artistix@gmail.com>
2024-04-30 17:39:09 +0500

    April 30, 2024

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,27 +8,27 @@ function Header() {
           <h1 className="text-4xl font-extrabold">VoIP</h1>
         </Link>
       </div>
-      <div className="flex flex-grow items-center justify-between gap-3 px-4">
+      <div className="flex flex-grow items-end justify-between gap-3 px-4">
         <div>
           <p>Account/Carrier</p>
           <input className="border border-b-k_light_gray p-1 "></input>
         </div>
         <div className="flex basis-3/4 justify-around gap-3">
-          <div className="basis-40">
+          <div className="grow basis-40">
             <div>Max CPS</div>
             <div className="border border-b-k_light_gray bg-[#D9D9D9] p-1">
               BOX
             </div>
           </div>
-          <div className="basis-32">
+          <div className="grow basis-32">
             <div>Max SESS</div>
             <div className="bg-[#D9D9D9] p-1">BOX</div>
           </div>
-          <div className="basis-32">
+          <div className="grow basis-32">
             <div>Blocked Account Cap</div>
             <div className="bg-[#D9D9D9] p-1">BOX</div>
           </div>
-          <div className="basis-32">
+          <div className="grow basis-32">
             <div>Blocked ID</div>
             <div className="bg-[#D9D9D9] p-1">BOX</div>
           </div>
