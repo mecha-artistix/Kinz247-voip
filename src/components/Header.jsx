c08e4e67c81e4d00c1a61db7adf73479e9cf798e
mecha-artistix <mecha.artistix@gmail.com>
2024-04-30 16:59:22 +0500

    30 April 2024

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,37 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="flex h-full w-full items-center justify-between border-b border-b-k_light_gray">
-      <div className="flex-shrink-0 grow-0  px-8">
+      <div className="width-32 flex-shrink-0 grow-0 px-8">
         <Link to="/">
           <h1 className="text-4xl font-extrabold">VoIP</h1>
         </Link>
       </div>
       <div className="header-menu flex flex-grow justify-between px-4">
-        <div>Input</div>
-        <div className="flex basis-3/4 justify-around">
-          <div>Box 1</div>
-          <div>Box 2</div>
-          <div>Box 3</div>
-          <div>Box 4</div>
+        <div>
+          <p>Account/Carrier</p>
+          <input className="border border-b-k_light_gray p-1"></input>
         </div>
-        <button className="hover:bg-k_blue_hover bg-k_blue">Button</button>
+        <div className="flex basis-3/4 justify-around ">
+          <div className="basis-40">
+            <div>Max CPS</div>
+            <div className="border border-b-k_light_gray p-1">BOX</div>
+          </div>
+          <div className="basis-32">
+            <div>Max SESS</div>
+            <div className="bg-[#D9D9D9] p-1">BOX</div>
+          </div>
+          <div className="">
+            <div>Blocked Account Cap</div>
+            <div className="bg-[#D9D9D9] p-1">BOX</div>
+          </div>
+          <div className="">
+            <div>Blocked ID</div>
+            <div className="bg-[#D9D9D9] p-1">BOX</div>
+          </div>
+        </div>
+        <button className="hover:bg-k_blue_hover w-[210px] rounded bg-k_blue py-1 text-white transition-all">
+          Filter
+        </button>
       </div>
     </div>
   );
